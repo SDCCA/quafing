648071d77f49c9cfaec97fe1123457e199538fbb
Francesco Nattino <f.nattino@esciencecenter.nl>
2022-04-01 12:17:26 +0200

    move tests to separate dir

--- fine/fine.py
+++ fine/fine.py
@@ -785,68 +785,6 @@ class FINE:
         return self._col_data
 
 
-##### UNIT TEST AREA #####
-
-def test_kl_distance_infinite_when_support_of_2nd_pdf_smaller_than_first():
-    p1 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    p2 = {1 : 0.2, 2 : 0.1, 4 : 0.7}
-    assert discrete_kl(p1, p2) == np.inf
-
-def test_kl_distance_zero_if_equal():
-    p1 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    p2 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    assert discrete_kl(p1, p2) == 0.0
-
-def test_hellinger_distance_zero_if_equal():
-    p1 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    p2 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    assert discrete_hellinger(p1, p2) == 0.0
-
-def test_hellinger_distance_is_symmetric():
-    p1 = {1 : 0.2, 2 : 0.1, 3 : 0.3, 4 : 0.4}
-    p2 = {1 : 0.2, 2 : 0.1, 4 : 0.7}
-    assert discrete_hellinger(p1, p2) == discrete_hellinger(p2, p1)
-
-def test_continuous_kl_converges_to_analytic_result():
-    from scipy.stats import norm
-    s1 = 1.0
-    s2 = 2.0
-    m1 = 1
-    m2 = 1
-    p1 = norm.freeze(loc=m1, scale=s1).pdf
-    p2 = norm.freeze(loc=m2, scale=s2).pdf
-    kl_dist = continuous_kl(p1, p2, base=None)
-    analytic = np.log(s2/s1) + (s1**2 + (m1-m2)**2) / (2 * (s2)**2) - 0.5
-    # On my machine relative error around 1e-16
-    assert (np.abs(kl_dist - analytic) / analytic) < 1e-4
-
-def test_continuous_hellinger_converges_to_analytic_result():
-    from scipy.stats import norm
-    s1 = 1.0
-    s2 = 2.0
-    m1 = 1
-    m2 = 1
-    p1 = norm.freeze(loc=m1, scale=s1).pdf
-    p2 = norm.freeze(loc=m2, scale=s2).pdf
-    hell_dist = continuous_hellinger(p1, p2)
-    analytic = 1 - np.sqrt((2*s1*s2)/(s1**2+s2**2)) * np.exp(-0.25*((m1-m2)**2)/(s1**2+s2**2))
-    analytic = np.sqrt(2 * analytic)
-    # On my machine relative error around 1e-16
-    assert (np.abs(hell_dist - analytic) / analytic) < 1e-4
-
-def test_cos_distance_zero_if_equal():
-    p1 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    p2 = {1 : 0.2, 2 : 0.1, 3 : 0.7}
-    assert discrete_cos(p1, p2) == 0.
-
-def test_cos_distance_with_analytic_result():
-    p1 = {1 : 1.}
-    p2 = {2 : 1.}
-    cos_dist = discrete_cos(p1, p2)
-    analytic = np.pi/2
-    assert (np.abs(cos_dist - analytic) / analytic) < 1e-4
-
-
 if __name__ == "__main__":
     f = FINE("/Users/eslt0101/Projects/SABM/testpy2/omri_subak_data.xlsx")
     #f = FINE("/home/omri/repos/research/subaks/in_sg/data/omri_subak_data.xlsx")
