37c0773d2fc16f40f195b3c852b2da7a16e61450
Meiert Grootes <m.grootes@esciencecenter.nl>
2022-05-13 13:27:44 +0200

    Added code generated by cookiecutter

---
+++ quafing/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about quafing"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Meiert Willem Grootes"
+__email__ = "m.grootes@esciencecenter.nl"
+__version__ = "0.1.0-alpha"
