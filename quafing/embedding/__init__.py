0ef5deeb9dd52000d5f0694a36bc5f66c8c9587c
Meiert Grootes <m.grootes@esciencecenter.nl>
2022-08-11 09:45:32 +0200

    added embedding methhods. WIP

---
+++ quafing/embedding/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from .mds_embedder import MDSEmbedding as mds
+
+embedders ={
+	'mds':mds
+}
+
+def get_embedder(method,mdpdf_collection=None):
+
+    _check_embedding_method(method)
+    embedder = embedders[method]
+    return embedder(mdpdf_collection)
+
+def _check_embedding_method(method):
+    if method not in embedders:
+        raise NotImplementedError(
+            f"Embedding algorithm {method} is unknown")	
+
+
+
+
+
+        
